2c072e0d0e5cc7b64baee0f7929248656f1e7a3b
Alex <suarez9093@hotmail.com>
2019-07-06 11:58:04 -0600

    styled login page

--- project_name/server.js
+++ project_name/server.js
@@ -17,6 +17,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+app.use(express.static("public"));
+
 
 // Handlebars
 const exphbs = require("express-handlebars");
