59a42690a9231188f2d53462b05351e3285a049d
Alex <suarez9093@hotmail.com>
2019-07-06 18:19:12 -0600

    completed the distributor page, form page using bulma

--- project_name/db/storeDB.sql
+++ project_name/db/storeDB.sql
@@ -11,6 +11,7 @@ CREATE TABLE `products` (
   `name` varchar(255) NOT NULL,
   `department` varchar(255) NOT NULL,
   `price` float(11,2) DEFAULT '0.00',
+  'description' varchar(255) NOT NULL,
   `stock` int(11) DEFAULT '0',
   `active` tinyint(1) DEFAULT '1',
   `createdAt` datetime NOT NULL,
@@ -20,17 +21,6 @@ CREATE TABLE `products` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `products`
---
-
-LOCK TABLES `products` WRITE;
-/*!40000 ALTER TABLE `products` DISABLE KEYS */;
-INSERT INTO products VALUES 
-(1,'titos','Vodka',9.99,10,1,'2017-11-01 03:20:53','2017-11-03 02:55:50'),
-(2,'Ciroc','Vodka',9.99,12,0,'2017-11-01 03:23:36','2017-11-03 02:56:03'),
-(3,'Grey Goose','Vodka',9.99,5,1,'2017-11-01 04:00:53','2017-11-03 15:04:12'),
-(4,'Bacardi','rum',9.99,10,0,'2017-11-01 04:01:52','2017-11-03 02:55:43'),
-(5,'Captain Morgan','rum',4.99,10,0,'2017-11-01 04:02:27','2017-11-03 02:55:56'),
-(6,'Bud light','beer',0.99,12,1,'2017-11-02 04:24:49','2017-11-03 02:34:39');
-/*!40000 ALTER TABLE `products` ENABLE KEYS */;
-UNLOCK TABLES;
+DROP DATABASE IF EXISTS `storedb`;
+CREATE DATABASE `storedb`;
+USE `storedb`;
--- project_name/public/assets/js/app.js
+++ project_name/public/assets/js/app.js
@@ -1,108 +1,103 @@
-$(document).ready(function(){
+$(document).ready(function () {
 	// datatable config
-    $('table').DataTable({
+	$('table').DataTable({
 		"columnDefs": [
-            { "orderable": false, "targets": [5] }
-          ],
-        order: [[ 0, "asc" ]],
-        info: false,
+			{ "orderable": false, "targets": [5] }
+		],
+		order: [[0, "asc"]],
+		info: false,
 		paging: true,
 		pagingType: "full_numbers"
-    });
-
-    // Validate the form before submit it
-	$('#submit').on('click', function(e){
-		e.preventDefault();
-		
-		if(validate())
-		{
-			$('form').submit();
-		} 
 	});
-    
 
-    // Delete button class onClick event. Those are the delete buttons inside the table
-    // Each of those buttons has a unique data-id attr
-    $(document).on("click", ".btn-delete", function(){
-        var id = $(this).attr("data-id"); //get the product id from the data-id
+	// Validate the form before submit it
+	$('#submit').on('click', function (e) {
+		e.preventDefault();
 
-        //Pass this id to the delete button in the confirm dialog box
-        $("#deleteProduct").attr("data-id", id);
+		if (validate()) {
+			$('form').submit();
+		}
+	});
 
-        //Show the dialog box
-        $("#confirm-delete").modal();
-    });
 
-    // This is the delete button in the confirm dialog box
-    $("#deleteProduct").on("click", function(){
-        var id = $(this).attr("data-id"); //get the product id from its data attr
+	// Delete button class onClick event. Those are the delete buttons inside the table
+	// Each of those buttons has a unique data-id attr
+	$(document).on("click", ".btn-delete", function () {
+		var id = $(this).attr("data-id"); //get the product id from the data-id
 
-        // Remove the record from the database
-        var url = '/' + id + '/delete'
-        $.post(url, function(){
-        	//Remove the row from the table
-        	$('#' + id).remove();
-        });
-    });
+		//Pass this id to the delete button in the confirm dialog box
+		$("#deleteProduct").attr("data-id", id);
 
-    // Validate user's input         
-    function validate(){
+		//Show the dialog box
+		$("#confirm-delete").modal();
+	});
 
-    	 // Remove all error messages and error classes
-        $('span.help-block').remove();
-        $('.has-error').removeClass('has-error');
+	// This is the delete button in the confirm dialog box
+	$("#deleteProduct").on("click", function () {
+		var id = $(this).attr("data-id"); //get the product id from its data attr
 
-        // Validate the name
-    	var name = $('#name');
-    	if(name.val().trim() === "")
-    	{
-    		displayError(name, 'Enter the title of the movie.');
-    		return false;
-    	}
+		// Remove the record from the database
+		var url = '/' + id + '/delete'
+		$.post(url, function () {
+			//Remove the row from the table
+			$('#' + id).remove();
+		});
+	});
 
-    	// Validate the department
-    	var department = $('#department');
-    	if(department.val().trim() === "")
-    	{
-    		displayError(department, 'Enter the genre.');
-    		return false;
-    	}
+	// Validate user's input         
+	function validate() {
 
-    	// Validate the price
-    	var price = $('#price');
-    	if(price.val().trim() != "" && !/^\d+[\.]?\d+$/.test(price.val().trim()))
-    	{
-    		displayError(price, 'Price must be a number 0 or greater.');
-    		return false;
-    	}
+		// Remove all error messages and error classes
+		$('span.help-block').remove();
+		$('.has-error').removeClass('has-error');
 
-    	// Validate the stock for 0 or a whole number greater than 0
-    	var stock = $('#stock');
-    	if(stock.val().trim() != "" && !/^\d+$/.test(stock.val().trim()))
-    	{
-    		displayError(stock, 'Stock must be 0 or a whole number greater than 0.');
-    		return false;
-    	}
-    	return true;
-    }            
+		// Validate the name
+		var name = $('#name');
+		if (name.val().trim() === "") {
+			displayError(name, 'Enter the title of the movie.');
+			return false;
+		}
 
-    // This function is called by validate to display an error message
+		// Validate the department
+		var department = $('#department');
+		if (department.val().trim() === "") {
+			displayError(department, 'Enter the genre.');
+			return false;
+		}
+
+		// Validate the price
+		var price = $('#price');
+		if (price.val().trim() != "" && !/^\d+[\.]?\d+$/.test(price.val().trim())) {
+			displayError(price, 'Price must be a number 0 or greater.');
+			return false;
+		}
+
+		// Validate the stock for 0 or a whole number greater than 0
+		var stock = $('#stock');
+		if (stock.val().trim() != "" && !/^\d+$/.test(stock.val().trim())) {
+			displayError(stock, 'Stock must be 0 or a whole number greater than 0.');
+			return false;
+		}
+		return true;
+	}
+
+	// This function is called by validate to display an error message
 	// if the input is invalid
-	function displayError(el, message){
+	function displayError(el, message) {
 		// Create a span element with class text-danger
 		var $span = $('<span class="help-block">');
 
 		// Add the message passed as argument to the new span
-	    $span.html('<strong>' + message + '<strong>');
+		$span.html('<strong>' + message + '<strong>');
 
-	    //Append the span after the input that is not valid
-	    el.after($span);
-	   
-	    // Set the focus 
-	    el.focus();
+		//Append the span after the input that is not valid
+		el.after($span);
 
-	    // Add class "has-error to the element's parent"
-	    el.parent().addClass('has-error');
+		// Set the focus 
+		el.focus();
+
+		// Add class "has-error to the element's parent"
+		el.parent().addClass('has-error');
 	}
 });
 
