1efb9cb27ca4bfdfefe15247575b036f9db1af55
Kalob <kalobcarter@Kalobs-MacBook-Pro.local>
2019-07-07 14:06:55 -0600

    retailer handlebars adding tiles

--- project_name/routes/product.js
+++ project_name/routes/product.js
@@ -21,17 +21,15 @@ router.get('/forgot', function(req, res) {
 });
 
 // Get request to get all products
-router.get('/distributor', function(req, res) {
-	res.render('list', {
-		loggedIn: true
-	}),
-	productController.list
-});
+router.get('/distributor', 	productController.list);
 
+// router.get('/distributor', productController.list);
 
 router.get("/retailer", function(req,res){
-	res.render("retailer");
-})
+	res.render("retailer", {
+		loggedIn: true
+	});
+});
 
 
 // GET request for insert a new product
