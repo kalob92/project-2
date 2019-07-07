1823d365cd79d8ee1c81cfc4e165a804507d80ab
Alex <suarez9093@hotmail.com>
2019-07-07 17:28:48 -0600

    working authenticaion to redirect

--- project_name/public/assets/js/login.js
+++ project_name/public/assets/js/login.js
@@ -1,12 +1,19 @@
-// $(document).ready(function() {
-//     $('#login.button').on('click', function(event) {
-//         event.preventDefault();
 
-        
-//     });
-// });
-
-$("#password-input").on("click", function(){
+$("#login").on("click", function (event) {
+    event.preventDefault();
     let username = $('#username-input').val().trim();
-    console.log(username);
+
+    if (username == "retailer") {
+        window.location.href = window.location.origin + "/retailer";
+    } else if (username == "distributor") {
+        window.location.href = window.location.origin + "/distributor";
+    }
+    else {
+        alert("Not a valid login!");
+        location.reload();
+    }
+
+
 });
+
+
